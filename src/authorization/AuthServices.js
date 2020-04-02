db6cfa54e5daf0c929baaf5651e685f96b3ead7d
WenAP <galaxy_315@hotmail.com>
2020-04-02 11:41:36 -0500

    feat(AUTH): Authorization services - integration ACL

--- src/authorization/AuthServices.js
+++ src/authorization/AuthServices.js
@@ -6,7 +6,7 @@ function authorizationServices({ path, method, servicesPath }) {
 		if (newSer.authorizationDefault) {
 			const methodPatch = `${method.toUpperCase()}..${path}`;
 			if (newSer.sales) {
-				return newSer.sales[methodPatch] ? true : false;
+				return !!newSer.sales[methodPatch];
 			}
 		}
 	}
